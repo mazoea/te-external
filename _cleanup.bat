e4393cd4f9c6b4cfe5f307565028c397db0cda70
jm <jm@server>
2016-08-03 19:52:27 +0200

    initial commit

---
+++ _cleanup.bat
@@ -0,0 +1,19 @@
+rm -rf freetype*
+rm -rf giflib*
+rm -rf jpeg*
+rm -rf libpng*
+rm -rf zlib*
+rm -rf libtiff*
+rm -rf tiff*
+
+rm -rf pango*
+rm -rf pixman*
+rm -rf cairo*
+rm -rf fontconfig*
+rm -rf libiconv*
+rm -rf libxml2*
+rm -rf glib*
+rm -rf harfbuzz*
+rm -rf icu*
+rm -rf libffi*
+pause
