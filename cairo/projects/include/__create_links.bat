e7459ed9a6ba054f5e73292f8fff21d2ab185318
jm <jm@server>
2016-08-05 17:08:47 +0200

    cairo projects

---
+++ cairo/projects/include/__create_links.bat
@@ -0,0 +1,10 @@
+junction freetype ..\..\..\..\te-external\freetype\include
+junction zlib ..\..\..\..\te-external\zlib\
+junction jpeg ..\..\..\..\te-external\jpeg\
+junction libtiff ..\..\..\..\te-external\libtiff\libtiff
+junction libpng ..\..\..\..\te-external\libpng\
+junction giflib ..\..\..\..\te-external\giflib\lib
+junction pixman ..\..\..\..\te-external\pixman\pixman
+junction fontconfig ..\..\..\..\te-external\fontconfig
+
+pause
---
+++ cairo/projects/libs/__create_links.bat
@@ -0,0 +1,10 @@
+junction zlib ..\..\..\..\te-external\zlib\libs
+junction jpeg ..\..\..\..\te-external\jpeg\libs
+junction libtiff ..\..\..\..\te-external\libtiff\libs
+junction libpng ..\..\..\..\te-external\libpng\libs
+junction giflib ..\..\..\..\te-external\giflib\libs
+junction pixman ..\..\..\..\te-external\pixman\libs
+junction fontconfig ..\..\..\..\te-external\fontconfig\libs
+junction freetype ..\..\..\..\te-external\freetype\libs
+
+pause
