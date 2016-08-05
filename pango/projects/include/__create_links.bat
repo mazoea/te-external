00013b2f89f6a4c676de52ccad9c4b8ae2344e5c
jm <jm@server>
2016-08-05 18:02:09 +0200

    pango projects

---
+++ pango/projects/include/__create_links.bat
@@ -0,0 +1,7 @@
+junction cairo ..\..\..\..\te-external\cairo\src
+junction glib ..\..\..\..\te-external\glib
+junction freetype ..\..\..\..\te-external\freetype\include
+junction fontconfig ..\..\..\..\te-external\fontconfig
+junction harfbuzz ..\..\..\..\te-external\harfbuzz\src
+
+pause
---
+++ pango/projects/libs/__create_links.bat
@@ -0,0 +1,10 @@
+junction cairo ..\..\..\..\te-external\cairo\libs
+junction libxml2 ..\..\..\..\te-external\libxml2\libs
+junction glib ..\..\..\..\te-external\glib\\libs
+junction freetype ..\..\..\..\te-external\freetype\libs
+junction fontconfig ..\..\..\..\te-external\fontconfig\libs
+junction harfbuzz ..\..\..\..\te-external\harfbuzz\libs
+junction libiconv ..\..\..\..\te-external\libiconv\libs
+junction pixman ..\..\..\..\te-external\pixman\libs
+
+pause
