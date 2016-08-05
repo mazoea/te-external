d0f71701617e75a0797a25434de71fbd3e87ca7c
jm <jm@server>
2016-08-05 20:19:23 +0200

    testing appveyor

--- create_links.py
+++ create_links.py
@@ -5,13 +5,32 @@ this_dir = os.path.dirname(os.path.abspath(__file__))
 
 execute = "__create_links.bat"
 
-dirs = glob.glob("*")
-for dir in dirs:
-    if "-" in dir:
-        continue
-    for root, dirs, files in os.walk(dir):
-        if execute in files:
-            print "Executing [%s] in [%s]" % (execute, root)
-            os.chdir(os.path.join(this_dir, root))
-            os.system("%s nopause" % execute )
-            os.chdir(this_dir)
+
+def _before_run_ci(root, batch):
+    with open(os.path.join(root, batch), mode="r") as fin:
+        t = fin.read()
+    subst_batch = batch + ".subst.bat"
+    t = t.replace("junction", "mklink /j")
+    with open(os.path.join(root, subst_batch), mode="w+") as fout:
+        fout.write(t)
+    return root, subst_batch
+
+
+def _run(root, batch):
+    if os.environ.get("APPVEYOR") is not None:
+        root, batch = _before_run_ci(root, batch) 
+    print "Executing [%s] in [%s]" % (batch, root)
+    os.chdir(os.path.join(this_dir, root))
+    os.system("%s nopause" % batch)
+    os.chdir(this_dir)
+
+
+if __name__ == '__main__':
+    for one_dir in glob.glob("*"):
+        # orig directories
+        if "-" in one_dir:
+            continue
+        # exe batches
+        for _root, _1, _files in os.walk(one_dir):
+            if execute in _files:
+                _run(_root, execute)
